e9d9ab9617c64d4f6050cc1c638af9abd6afaf63
Alexkawai <leha.iuganov@yandex.ru>
2022-02-06 13:43:42 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
-import React, { PureComponent, useState } from 'react';
+import React, { PureComponent, useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const updateItem = (issue_number, state) => {
+  fetch(`https://api.github.com/repos/Alexkawai/CanbanBoardPractices/issues/${issue_number}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Basic ${btoa('Alexkawai' + ':' + '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')}`
+    },
+    body: JSON.stringify({
+      "owner": 'Alexkawai',
+      "repo": 'CanbanBoardPractices',
+      "issue_number": issue_number,
+      "state": state
+    })
+})
+};
+
+const loadItem = (setColumns) => {
+  fetch('https://api.github.com/repos/Alexkawai/CanbanBoardPractices/issues')
+    .then((response) => response.json())
+    .then((data) =>
+      data.map((item) => ({
+        id: `${item.number}`,
+        content: item.title,
+        data,
+        state: item.state,
+      }))
+    )
+    .then((data) => {
+      const columns = {
+        open: {
+          name: 'Open',
+          items: [],
+        },
+        close: {
+          name: 'Close',
+          items: [],
+        },
+      };
+      for (let key of data
+        .map((el) => el.state)
+        .filter((value, index, array) => array.indexOf(value) === index)) {
+        columns[key] = {
+          name: key,
+          items: data.filter((el) => el.state === key),
+        };
+      }
+
+      // console.log(columns);
+      return columns;
+    })
+    .then((data) => setColumns(data));
+};
 
 // fake data generator for replace to map data  from GET https://api.github.com/repos/{owner}/{repo}/issues
 const getItems = (count) =>
@@ -13,7 +66,7 @@ const columnsFromBackend = {
     name: 'Open',
     items: getItems(10),
   },
-  
+
   close: {
     name: 'Close',
     items: [],
@@ -23,7 +76,7 @@ const columnsFromBackend = {
 const onDragEnd = (result, columns, setColumns) => {
   if (!result.destination) return;
   const { source, destination } = result;
-
+  
   if (source.droppableId !== destination.droppableId) {
     const sourceColumn = columns[source.droppableId];
     const destColumn = columns[destination.droppableId];
@@ -55,11 +108,14 @@ const onDragEnd = (result, columns, setColumns) => {
       },
     });
   }
+  console.log(result,columns)
 };
 
 export default function App() {
   const [columns, setColumns] = useState(columnsFromBackend);
-
+  useEffect(() => {
+    loadItem(setColumns)
+  },[]);
   return (
     <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
       <DragDropContext
